4727494398a7927cfd41339c7bec2d83dcd43a44
PerDriveFast <94453634+PerDriveFast@users.noreply.github.com>
2025-09-14 16:50:53 +0700

    first commit

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+$dbhost = 'localhost';
+$dbname = 'ecommerce_project';
+$dbuser = 'root';
+$dbpass = 'thanhtrung123@#Z';
+try {
+    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $exception) {
+    echo "Connection error :" . $exception->getMessage();
+}
+define("BASE_URL", "http://localhost:3000/");
+define("ADMIN_URL", BASE_URL . "admin/");
+
+define("SMTP_HOST", "sandbox.smtp.mailtrap.io");
+define("SMTP_PORT", "587");
+define("SMTP_USERNAME", "f08fef70f919e3");
+define("SMTP_PASSWORD", "301cd3a7b4f466");
+define("SMTP_ENCRYPTION", "tls");
+define("SMTP_FROM", "admin@example.com");
